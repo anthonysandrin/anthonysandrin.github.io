b8e1976b72fa7fc3c0df8fc862405ef2892a8f82
Anthony Sandrin <sandrinanthony@gmail.com>
2019-04-01 20:50:37 +0000

    CS488

---
+++ cs488/js/cs488.js
@@ -0,0 +1,11 @@
+images = document.getElementsByClassName('zooming-image')
+
+for (var i = 0; i < images.length; i++) {
+  d = new Drift(images.item(i) ,{
+    inlinePane: true,
+    containInline: true,
+    zoomFactor: 3,
+    sourceAttribute: 'src'
+  });
+}
+
